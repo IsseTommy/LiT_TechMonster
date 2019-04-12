31d386b1b14c2269419f36a614d9153b6e138e12
IsseTommy <0jofePfN>
2019-04-12 09:01:43 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by Tommy on 2019/04/12.
+//  Copyright © 2019 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
